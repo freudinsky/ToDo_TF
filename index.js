39f03a8a0f08124b1ba0f33a02e629ca1d498791
freudinsky <136381953+freudinsky@users.noreply.github.com>
2023-08-21 15:28:55 +0200

    ..

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Offene ToDos: 
+// Offene ToDos:
 //Check-Boxen => bei Done CSS Class für Done + in 2. UL verschieben
 //CSS/Design
 //Animationen und anderer Stuff der mir noch spontan so einfällt
@@ -7,7 +7,7 @@
 
 const form = document.querySelector("#todo-form");
 const ulToDo = document.querySelector("#todo-ul");
-const ulComplete = document.querySelector("#complete-ul")
+const ulComplete = document.querySelector("#complete-ul");
 
 // To-Do Liste selbst
 
@@ -16,9 +16,13 @@ let toDos = [
 	{ name: "Saufen", completed: false, id: 222 },
 	{ name: "Shoppen gehen", completed: false, id: 333 },
 ];
-localStorage.setItem("ToDos", JSON.stringify(toDos))
-const localToDos = JSON.parse(localStorage.getItem("ToDos"))
-localToDos.forEach(item => renderList(item)) // First render
+
+// Local Storage-Stuff -> local storage kann keine Arrays etc. speichern, daher muss das in JSON-Formatierung umgewandelt werden
+
+const localToDos = localStorage.getItem("ToDos")
+	? JSON.parse(localStorage.getItem("ToDos"))
+	: [...toDos];
+localToDos.forEach((item) => renderList(item));
 
 // Liste rendern
 
@@ -31,21 +35,19 @@ function renderList(todo) {
     <label for="${todo.id}"><p>${todo.name}</p></label>
     <button class="delete-todo">Delete</button>`;
 
-    ulToDo.append(li) // if/else für completed/non completed-aufteilung
+	ulToDo.append(li); // if/else für completed/non completed-aufteilung
 }
 
 // Delete + Done
 
 function deleteToDo(id) {
-    toDos = toDos.filter(item => item.id !== Number(id))
-    localStorage.clear()
-    localStorage.setItem("ToDos", JSON.stringify(toDos))
-    const li = document.getElementById(id)
-    ulToDo.removeChild(li)
+	toDos = toDos.filter((item) => item.id !== Number(id));
+	localStorage.clear();
+	localStorage.setItem("ToDos", JSON.stringify(toDos));
+	const li = document.getElementById(id);
+	ulToDo.removeChild(li);
 }
-function toggleDone(id) {
-    toDos
-}
+function toggleDone(id) {}
 
 // Todos hinzufügen
 
@@ -56,8 +58,8 @@ function addNewToDo(name) {
 		id: Date.now(),
 	};
 	toDos.push(newToDo);
-    localStorage.setItem("ToDos", JSON.stringify(toDos))
-    renderList(newToDo);
+	localStorage.setItem("ToDos", JSON.stringify(toDos));
+	renderList(newToDo);
 }
 
 form.addEventListener("submit", (click) => {
@@ -76,9 +78,19 @@ form.addEventListener("submit", (click) => {
 // Delete-Button
 
 ulToDo.addEventListener("click", (event) => {
-    if (event.target.classList.contains("delete-todo")){
-        const parent = event.target.parentElement;
-        const id = parent.getAttribute("id")
-        deleteToDo(id)
-    }
-})
+	if (event.target.classList.contains("delete-todo")) {
+		const parent = event.target.parentElement;
+		const id = parent.getAttribute("id");
+		deleteToDo(id);
+	}
+});
+
+// Completed-Checkbox
+
+ulToDo.addEventListener("click", (event) => {
+	if (event.target.classList.contains("completed-false")) {
+		const parent = event.target.parentElement;
+		const id = parent.getAttribute("id");
+		deleteToDo(id);
+	}
+});
